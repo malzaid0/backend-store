3970eb1e48614f82461af678f070b77dc7ad038d
Mohammed Alzaid <m.al-zaid@live.com>
2020-12-29 18:33:07 +0300

    some cleaning

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import Category, Product, Image, Country, Address, Order, OrderItem
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "inventory", "date_added", "category"]
+    search_fields = ["name", "category__name"]
+    list_filter = ["category"]
+    list_editable = ["price"]
+
+
+admin.site.register(Product, ProductAdmin)
+
 admin.site.register(Image)
 admin.site.register(Country)
 admin.site.register(Address)
